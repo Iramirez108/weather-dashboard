a2c85c6139c8a3b0fc3dd8fab9682f775828c287
Iramirez108 <ivan.ramirez10208@gmail.com>
2022-07-21 22:54:21 -0500

    update js

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # weather-dashboard
+
+Weather app dashboard that is meant to give you the weather of a city you desire.
+
+Used the API of https://openweathermap.org/
+
--- script.js
+++ script.js
@@ -11,50 +11,146 @@
 //WHEN I click on a city in the search history
 //THEN I am again presented with current and future conditions for that city
 
-var apiKey = "1d0652383071067233fde937e77bd6e5";
+var apiKey = "165b646ccac26274ed2c218ff1b3a17b";
 var today = moment().format('L');
 var searchHistoryList = [];
 
-function currentCondition(city){
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+function currentCondition(city) {
+
+  var queryURL = `https://api.openweathermap.org/data/3.0/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).then(function(cityWeatherResponse) {
+      console.log(cityWeatherResponse);
+      
+      $("#weatherContent").css("display", "block");
+      $("#cityDetail").empty();
+      
+      var iconCode = cityWeatherResponse.weather[0].icon;
+      var iconURL = `https://openweathermap.org/img/w/${iconCode}.png`;
+      
+      var currentCity = $(`
+        <h2 id="currentCity">
+            ${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}" />
+        </h2>
+        <p>Temperature: ${cityWeatherResponse.main.temp} °F</p>
+        <p>Humidity: ${cityWeatherResponse.main.humidity}\%</p>
+        <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>
+    `);
+
+    $("#cityDetail").append(currentCity);
+
+
+        var lat = cityWeatherResponse.coord.lat;
+        var lon = cityWeatherResponse.coord.lon;
+        var uviQueryURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=${part}&appid=${apiKey}`;
+
+        $.ajax({
+            url: uviQueryURL,
+            method: "GET"
+        }).then(function(uviResponse) {
+            console.log(uviResponse);
+
+            var uvIndex = uviResponse.value;
+            var uvIndexP = $(`
+                <p>UV Index: 
+                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+                </p>
+            `);
+
+            $("#cityDetail").append(uvIndexP);
+
+            futureCondition(lat, lon);
+
+            if (uvIndex >= 0 && uvIndex <= 2) {
+              $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+          } else if (uvIndex >= 3 && uvIndex <= 5) {
+              $("#uvIndexColor").css("background-color", "#FFF300");
+          } else if (uvIndex >= 6 && uvIndex <= 7) {
+              $("#uvIndexColor").css("background-color", "#F18B00");
+          } else if (uvIndex >= 8 && uvIndex <= 10) {
+              $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+          } else {
+              $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+          };  
+      });
+  });
+}
+
+
+function futureCondition(lat, lon) {
+
+    var futureURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=current,minutely,hourly,alerts&appid=${apiKey}`;
+
     $.ajax({
-        url:queryURL,
-        method:"GET"
-    }).then(function(cityWeatherResponse){
-        console.log(cityWeatherResponse);
-        $("#weatherContent").css("display","block");
-        $("#cityDetail").empty();
-        var iconCode = cityWeatherResponse.weather[0].icon;
-        var iconURL = 'https://openweathermap.org/img/w/${iconCode}.png';
+        url: futureURL,
+        method: "GET"
+    }).then(function(futureResponse) {
+        console.log(futureResponse);
+        $("#fiveDay").empty();
+        
+        for (let i = 1; i < 6; i++) {
+            var cityInfo = {
+                date: futureResponse.daily[i].dt,
+                icon: futureResponse.daily[i].weather[0].icon,
+                temp: futureResponse.daily[i].temp.day,
+                humidity: futureResponse.daily[i].humidity
+            };
 
-        var currentCity = $('<h2 id="currentCity">${cityWeatherResponse.name} ${today} <img src="${iconURL}" alt="${cityWeatherResponse.weather[0].description}"/></h2> <p>Temperature: ${cityWeatherResponse.main.temp} °F</p><p>Humidity: ${cityWeatherResponse.main.humidity}\%</p> <p>Wind Speed: ${cityWeatherResponse.wind.speed} MPH</p>');
+            var currDate = moment.unix(cityInfo.date).format("MM/DD/YYYY");
+            var iconURL = `<img src="https://openweathermap.org/img/w/${cityInfo.icon}.png" alt="${futureResponse.daily[i].weather[0].main}" />`;
 
-        $("#cityDetail").append(currentCity);
 
-        futureCondition(lat,lon);
-    });
-};
+            var futureCard = $(`
+                <div class="pl-3">
+                    <div class="card pl-3 pt-3 mb-3 bg-primary text-light" style="width: 12rem;>
+                        <div class="card-body">
+                            <h5>${currDate}</h5>
+                            <p>${iconURL}</p>
+                            <p>Temp: ${cityInfo.temp} °F</p>
+                            <p>Humidity: ${cityInfo.humidity}\%</p>
+                        </div>
+                    </div>
+                <div>
+            `);
 
-//got code from https://www.openuv.io/uvindex
-function getUVIndex() {
-    var lat = $('#lat').val();
-    var lng = $('#lng').val();
-    var alt = $('#alt').val();
-    var ozone = $('#ozone').val();
-    var dt = $('#dt').val();
-   
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      beforeSend: function(request) {
-        request.setRequestHeader('x-access-token', '1d60f2c9bd5146e2b08fb7ba82574d66');
-      },
-      url: 'https://api.openuv.io/api/v1/uv?lat=' + lat + '&lng=' + lng + '&alt=' + alt + '&ozone=' + ozone + '&dt=' + dt,
-      success: function(response) {
-        //handle successful response
-      },
-      error: function(response) {
-        // handle error response
-      }
-    });
-   }
+            $("#fiveDay").append(futureCard);
+        }
+    }); 
+}
+
+$("#searchBtn").on("click", function(event) {
+  event.preventDefault();
+
+  var city = $("#enterCity").val().trim();
+  currentCondition(city);
+  if (!searchHistoryList.includes(city)) {
+      searchHistoryList.push(city);
+      var searchedCity = $(`
+          <li class="list-group-item">${city}</li>
+          `);
+      $("#searchHistory").append(searchedCity);
+  };
+  
+  localStorage.setItem("city", JSON.stringify(searchHistoryList));
+  console.log(searchHistoryList);
+});
+
+
+$(document).on("click", ".list-group-item", function() {
+  var listCity = $(this).text();
+  currentCondition(listCity);
+});
+
+$(document).ready(function() {
+  var searchHistoryArr = JSON.parse(localStorage.getItem("city"));
+
+  if (searchHistoryArr !== null) {
+      var lastSearchedIndex = searchHistoryArr.length - 1;
+      var lastSearchedCity = searchHistoryArr[lastSearchedIndex];
+      currentCondition(lastSearchedCity);
+      console.log(`Last searched city: ${lastSearchedCity}`);
+  }
+});
